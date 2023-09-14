8ac1fd5f1a0ee9f5ac0515227d693100d4a1d305
0svaldojunior <app.o.r.jr@gmail.com>
2023-09-14 19:56:22 -0300

    feat: axios, seed for DB and configure header response

--- api/src/routes/generate-ai-completion.ts
+++ api/src/routes/generate-ai-completion.ts
@@ -1,3 +1,4 @@
+import { OpenAIStream, streamToResponse } from 'ai'
 import { FastifyInstance } from "fastify"
 import { z } from 'zod'
 import { openai } from "../lib/openai"
@@ -30,9 +31,17 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       temperature,
       messages: [
         { role: 'user', content: promptMessage },
-      ]
+      ],
+      stream: true
     })
 
-    return response
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      }
+    })
   })
 }
